57e52253311576bbdfd4f3b718f42281f7d5e27d
limason <47013087+limason@users.noreply.github.com>
2020-09-10 21:17:05 +0200

    Update ex1_interupt.s

--- ex1_interupt.s
+++ ex1_interupt.s
@@ -83,7 +83,7 @@
 .globl  _reset
 .type   _reset, %function
 .thumb_func
-_reset:
+_reset: 
 	bl GPIO_CLOCK_ENABLE
 
         // Load the base adresses for PA and PC into registers
@@ -91,31 +91,23 @@ _reset:
         ldr r2, = GPIO_PC_BASE
 
         //LED
-
         // Set high drive strength
-        mov r4, #2
+        mov r4, #0x2
         str r4, [r1, #GPIO_CTRL]
 
         // Set pins 8-15 to output
         ldr r3, = 0x55555555
         str r3, [r1, #GPIO_MODEH]
-
+        
         //BUTTONS
-
-    // Set pins 0-7 to input
+	// Set pins 0-7 to input
         ldr r3, = 0x33333333
         str r3, [r2, #GPIO_MODEL]
 
         // Enable internal pull-up
         ldr r3, = 0xff
         str r3, [r2, #GPIO_DOUT]
-    
-    // Load the output for PA and input for PC into registers
-        ldr r3, [r1, #GPIO_DOUT]
-        ldr r4, [r2, #GPIO_DIN]
-
-
-
+	
 	// Interrupt
 	ldr r5, = GPIO_BASE
 
@@ -132,53 +124,57 @@ _reset:
 	ldr r6, = 0x802
 	ldr r7, = ISER0
 	str r6, [r7]
-
+		
+	// Sleep modes
+	mov r10, #6
+	ldr r11, = SCR
+	str r10, [r11]
+	wfi
+		
+		b .  // do nothing
+	
+	/////////////////////////////////////////////////////////////////////////////
+	//
+  // GPIO handler
+  // The CPU will jump here when there is a GPIO interrupt
+	//
+	/////////////////////////////////////////////////////////////////////////////
 GPIO_CLOCK_ENABLE:
-		    // Enable GPIO clock
-	ldr r1, = CMU_BASE
-	ldr r2, [r1, #CMU_HFPERCLKEN0]
-	mov r3, #1
-	lsl r3, r3, #CMU_HFPERCLKEN0_GPIO
-	orr r2, r3, r2
-	str r2, [r1, #CMU_HFPERCLKEN0]
-	mov r15, r14
+        // Set up clock
+        ldr r1, = CMU_BASE
+        ldr r2, [r1, #CMU_HFPERCLKEN0]
+        mov r3, #1
+        lsl r3, r3, #CMU_HFPERCLKEN0_GPIO
+        orr r2, r2, r3
+        str r2, [r1, #CMU_HFPERCLKEN0]
+        mov r15, r14
 
-	b .  // do nothing
-
-	/////////////////////////////////////////////////////////////////////////////
-	//
-	// GPIO handler
-	// The CPU will jump here when there is a GPIO interrupt
-	//
-	/////////////////////////////////////////////////////////////////////////////
-
-	.thumb_func
-	gpio_handler:
+        .thumb_func
+gpio_handler:  
 	ldr r5, = GPIO_BASE
-
+	ldr r1, = GPIO_PA_BASE
+   	ldr r2, = GPIO_PC_BASE
+		
 	// Determine the source of the interrupt
 	ldr r6, [r5, #GPIO_IF]
 
 	// Clear the interrupt
 	str r6, [r5, #GPIO_IFC]
 
-	ldr r1, = GPIO_PA_BASE
-	ldr r2, = GPIO_PC_BASE
-	ldr r3, [r1, #GPIO_DOUT]
-	ldr r4, [r2, #GPIO_DIN]
-
-	ldr r5, [r4]
-	lsl r5, r5, #8
-	str r5, [r3]
-
-
-	b .  // do nothing
-
+	ldr r7, [r2, #GPIO_DIN]
+	lsl r7, r7, #8
+	str r7, [r1, #GPIO_DOUT]
+	bx r14
+		
+		b .  // do nothing
+	
 	/////////////////////////////////////////////////////////////////////////////
+	
+        .thumb_func
+dummy_handler:  
+        b .  // do nothing
 
-	.thumb_func
-	dummy_handler:
-	b .  // do nothing
+
 
 
 
