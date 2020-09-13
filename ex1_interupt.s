ffb597342ce7a87d786a343e92ae8c11a14359ea
limason <47013087+limason@users.noreply.github.com>
2020-09-13 16:01:50 +0200

    Update ex1_interupt.s

--- ex1_interupt.s
+++ ex1_interupt.s
@@ -1,4 +1,4 @@
-        .syntax unified
+              .syntax unified
 	
 	      .include "efm32gg.s"
 
@@ -71,101 +71,105 @@
 
 	      .section .text
 
+	/////////////////////////////////////////////////////////////////////////////
+	//
+	// Reset handler
+  // The CPU will start executing here after a reset
+	//
+	/////////////////////////////////////////////////////////////////////////////
 
-
-/////////////////////////////////////////////////////////////////////////////
-//
-// Reset handler
-// The CPU will start executing here after a reset
-//
-/////////////////////////////////////////////////////////////////////////////
-
-.globl  _reset
-.type   _reset, %function
-.thumb_func
+	      .globl  _reset
+	      .type   _reset, %function
+        .thumb_func
 _reset: 
-	bl GPIO_CLOCK_ENABLE
-
-        // Load the base adresses for PA and PC into registers
-        ldr r1, = GPIO_PA_BASE
+		bl GPIO_clock_enable
+		
+		ldr r0, = GPIO_BASE                 /* Load the base adress for GPIO into register */
+        ldr r1, = GPIO_PA_BASE              /* Load the base adresses for PA and PC into registers */
         ldr r2, = GPIO_PC_BASE
-
-        //LED
-        // Set high drive strength
-        mov r4, #0x2
-        str r4, [r1, #GPIO_CTRL]
-
-        // Set pins 8-15 to output
-        ldr r3, = 0x55555555
-        str r3, [r1, #GPIO_MODEH]
         
-        //BUTTONS
-	// Set pins 0-7 to input
-        ldr r3, = 0x33333333
+		bl LED_set_up
+		bl button_set_up
+		bl enable_interrupt
+		bl enable_sleep
+		bl power_down_RAM_blocks
+		loop:
+			wfi
+		b loop                           /* Go to sleep */
+		
+	    b .  // do nothing
+	
+
+GPIO_clock_enable:
+        ldr r1, = CMU_BASE                         /* Load CMU base address */
+        ldr r2, [r1, #CMU_HFPERCLKEN0]             /* Load value of CMU_HFPERCLKEN0 */
+        mov r3, #1                                 /* Set bit for GPIO clk */
+        lsl r3, r3, #CMU_HFPERCLKEN0_GPIO
+        orr r2, r2, r3
+        str r2, [r1, #CMU_HFPERCLKEN0]             /* Store new value of CMU_HFPERCLKEN0 */
+        mov r15, r14                               /* Move the value of lr into pc */
+
+LED_set_up:
+        mov r4, #0x3                              /* Set drive strength high(20mA) when 2, low when 3 (2mA) */
+        str r4, [r1, #GPIO_CTRL]
+        ldr r3, = 0x55555555                       /* Set pins 8-15 to output */
+        str r3, [r1, #GPIO_MODEH]
+        mov r15, r14                               /* Move the value of lr into pc */
+
+button_set_up:
+        ldr r3, = 0x33333333                       /* Set pin 0-7 to input */
         str r3, [r2, #GPIO_MODEL]
-
-        // Enable internal pull-up
-        ldr r3, = 0xff
+        ldr r3, = 0xff                             /* Enable internal pull-up */
         str r3, [r2, #GPIO_DOUT]
+        mov r15, r14                               /* Move the value of lr into pc */
 	
-	// Interrupt
-	ldr r5, = GPIO_BASE
-
-	// 
-	ldr r6, = 0x22222222
-	str r6, [r5, #GPIO_EXTIPSELL]
-
-	// Set interrupt on 1->0 transition, set interrupt on 0->1 transition and enable interrupt generation
-	ldr r6, = 0xff
-	str r6, [r5, #GPIO_EXTIFALL]
-	str r6, [r5, #GPIO_EXTIRISE]
-	str r6, [r5, #GPIO_IEN]
-
-	ldr r6, = 0x802
-	ldr r7, = ISER0
-	str r6, [r7]
+enable_interrupt:
+		ldr r6, = 0x22222222                       /* Set interrupt for port C */
+		str r6, [r0, #GPIO_EXTIPSELL]
+		ldr r6, = 0xff
+		str r6, [r0, #GPIO_EXTIFALL]               /* Set interrupt on 1->0 transition */
+		str r6, [r0, #GPIO_EXTIRISE]               /* Set interrupt on 0->1 transition */
+		str r6, [r0, #GPIO_IEN]                    /* Enable interrupt generation */
 		
-	// Sleep modes
-	mov r10, #6
-	ldr r11, = SCR
-	str r10, [r11]
-	wfi
-		
-		b .  // do nothing
+		ldr r6, = 0x802                            /* Enable interrupt handler */
+		ldr r7, = ISER0
+		str r6, [r7]
+        mov r15, r14                               /* Move the value of lr into pc */
 	
+enable_sleep:
+		mov r10, #6                            /* Enable SLEEPDEEP and SLEEPONEXIT */ 
+		ldr r11, = SCR
+		str r10, [r11]
+        mov r15, r14                               /* Move the value of lr into pc */
+        
+power_down_RAM_blocks:
+		ldr r9, = EMU_BASE
+		mov r10, #7
+		str r10, [r9, #EMU_MEMCTRL]
+ 	        mov r15, r14    
+
+
 	/////////////////////////////////////////////////////////////////////////////
 	//
   // GPIO handler
   // The CPU will jump here when there is a GPIO interrupt
 	//
 	/////////////////////////////////////////////////////////////////////////////
-GPIO_CLOCK_ENABLE:
-        // Set up clock
-        ldr r1, = CMU_BASE
-        ldr r2, [r1, #CMU_HFPERCLKEN0]
-        mov r3, #1
-        lsl r3, r3, #CMU_HFPERCLKEN0_GPIO
-        orr r2, r2, r3
-        str r2, [r1, #CMU_HFPERCLKEN0]
-        mov r15, r14
 
         .thumb_func
 gpio_handler:  
-	ldr r5, = GPIO_BASE
-	ldr r1, = GPIO_PA_BASE
-   	ldr r2, = GPIO_PC_BASE
 		
-	// Determine the source of the interrupt
-	ldr r6, [r5, #GPIO_IF]
+		// Determine the source of the interrupt
+		ldr r6, [r0, #GPIO_IF]
 
-	// Clear the interrupt
-	str r6, [r5, #GPIO_IFC]
-
-	ldr r7, [r2, #GPIO_DIN]
-	lsl r7, r7, #8
-	str r7, [r1, #GPIO_DOUT]
-	bx r14
+		// Clear the interrupt
+		str r6, [r0, #GPIO_IFC]
+		ldr r7, [r2, #GPIO_DIN]
+    	lsl r7, r7, #8
+    	str r7, [r1, #GPIO_DOUT]
+		bx r14
 		
+
 		b .  // do nothing
 	
 	/////////////////////////////////////////////////////////////////////////////
@@ -173,16 +177,12 @@ gpio_handler:
         .thumb_func
 dummy_handler:  
         b .  // do nothing
-
-
-
-
-
-
-
-
-
-
-
-
-
+      
+      
+      
+      
+      
+      
+      
+      
+   
