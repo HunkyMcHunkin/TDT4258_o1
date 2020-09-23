fb88ef080bcad056152fd3c6d6cce3853507d107
TDT4258 Student <tdt4258@tdt4258-VirtualBox.(none)>
2020-09-23 14:07:11 +0200

    added exercice 2

---
+++ ex2_support/timer.c
@@ -0,0 +1,40 @@
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "efm32gg.h"
+
+/*
+ * function to setup the timer 
+ */
+void setupTimer(uint16_t period)
+{
+	/*
+	 * TODO enable and set up the timer
+	 * 
+	 * 1. Enable clock to timer by setting bit 6 in CMU_HFPERCLKEN0 
+	 * 2. Write the period to register TIMER1_TOP 
+	 * 3. Enable timer interrupt generation by writing 1 to TIMER1_IEN 
+	 * 4. Start the timer by writing 1 to TIMER1_CMD
+	 * 
+	 * This will cause a timer interrupt to be generated every (period)
+	 * cycles. Remember to configure the NVIC as well, otherwise the
+	 * interrupt handler will not be invoked. 
+	 */
+	 *CMU_HFPERCLKEN0 = *CMU_HFPERCLKEN0 | (1<<6);
+	 *TIMER1_TOP = period;
+	 *TIMER1_IEN = 1;
+	 *ISERO = *ISERO | (1<<12);
+	 *TIMER1_CMD = 1; 
+}
+
+void poolTime(uint16_t period)
+{
+	if *TIMER1_CNT = period
+	{
+		LETIMER1_CMD = LETIMER1_CMD | (1<<2);
+		dacStop(void);
+	}
+	else{
+		*TIMER1_CMD = 0;
+		 
+		
