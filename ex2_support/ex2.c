efdcd6d756718bdac0aceb4e9bbf99aa2cce0edd
TDT4258 Student <tdt4258@tdt4258-VirtualBox.(none)>
2020-10-14 11:00:59 +0200

    Added button debouncing in ex2.c, hopefully

--- ex2_support/ex2.c
+++ ex2_support/ex2.c
@@ -16,6 +16,7 @@ main (void)
   int *wave;
   *wave = 3;
   uint32_t button_in;
+  bool past_button_state = 1;
 
   //setup configurations
   setup_GPIO ();
@@ -27,9 +28,20 @@ main (void)
   //operate on buttonpress
   while (1)
     {
-
       button_in = *GPIO_PC_DIN;
-      buttonPressed (button_in, wave);
-
+      // Enable the buttonPressed() every time a button is pressed.
+      // If the button haven't been pressed before, and it is being pressed..
+      // .. enable buttonPressed() and set past_button_state = 0.
+      if (past_button_state == 1 && button_in == 0xff)
+      {
+	past_button_state = 0;
+	buttonPressed(button_in, wave);
+      }
+      // Reset the past state when the button is released
+      if (button_state != 0xff)
+      {
+        past_button_state = 1;
+      }
     }
 }
+
