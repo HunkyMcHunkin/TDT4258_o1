3085e0eb236b6f7f7f24e42987a0ce28f4b42eed
TDT4258 Student <tdt4258@tdt4258-VirtualBox.(none)>
2020-11-21 10:14:15 +0100

    Please work

--- ex3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/graphics.h
+++ ex3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/graphics.h
@@ -11,7 +11,7 @@
 
 #define Black		0x0000
 #define White 		0xFFFF
-#define LightGrey       0xC618
+#define LightGrey   0xC618
 #define Red 		0xF000
 #define Blue 		0X000F
 
@@ -20,6 +20,6 @@ int init_framebuffer();
 void deinit_framebuffer();
 void refresh_fb();
 void draw_grid();
-void fill_block(int maxR, int minR, int maxC, int minC, int player);
+void fill_block(int maxR, int minR, int maxC, int minC, bool player);
 int get_row(int pixel);
 int get_col(int pixel, int row);
